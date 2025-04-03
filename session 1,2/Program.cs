c7a83959f4342fa0da5f2f5ec0f635f2b1ac6959
Sujal Shinde <88922332+sujalshinde@users.noreply.github.com>
2025-04-03 14:14:30 +0530

    c-sharp session 1 & 2

---
+++ session 1,2/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//implicit type casting
+int a = 9;
+double b = a;
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+//explicit type casting
+double c = 78.90;
+int d = (int)c;
+Console.WriteLine(d);
+
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine("Hello " +name);
+
+int num1 = 70;
+int num2 = 10;
+Console.WriteLine(num1+num2);
+Console.WriteLine(num1-num2);
+Console.WriteLine(num1*num2);
+Console.WriteLine(num1/num2);
+
+Console.WriteLine("Enter your Age: ");
+string age = Console.ReadLine();
+
+Console.WriteLine("Enter your Country: ");
+string country = Console.ReadLine();
+
+int agenum = int.Parse(age);
+
+if(agenum>=18 && country == "India")
+{
+Console.WriteLine("Hello, you are eligible");
+}
+else
+{
+    Console.WriteLine("Sorry, you are not eligible");
+
+}
+
+
+Console.WriteLine(Math.Max(20, 29));
+Console.WriteLine(Math.Min(20, 29));
+Console.WriteLine(Math.Sqrt(25));
+Console.WriteLine(Math.Abs(-566));
+Console.WriteLine(Math.Round(89.30));
+
+
+string str1 = "Revolution";
+string str2 = "IT";
+Console.WriteLine(str1.Length);
+Console.WriteLine(str1+" "+str2);
+
+int x = 30;
+int y = 90;
+Console.WriteLine(x<y);
+
+
+
+
+
+
+
+
+
+
